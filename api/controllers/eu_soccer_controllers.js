4c07b17d0d2149a42611cf4b92fbc5635dcb8f75
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-06-08 10:28:33 -0400

    added npm scripts

--- api/controllers/eu_soccer_controllers.js
+++ api/controllers/eu_soccer_controllers.js
@@ -1,5 +1,21 @@
 'use strict';
 
-exports.leagues = (req, res) => {
-  console.log('leagues');
+const Soccer = require('../models/soccer_models');
+
+const errMsg = {message: 'err'};
+
+exports.getLeagues = async (req, res) => {
+  console.log('get leagues...');
+  let leagues = await Soccer.league.find();
+  res.send(leagues);
+};
+
+exports.getOneLeague = async (req, res) => {
+  let option = req.body;
+  let league = await Soccer.league.findOne(option, (err, league) => {
+    if (err) res.send(errMsg);
+    return league;
+  });
+  if (!league) res.send({message: 'Not found.'});
+  res.send(league);
 };
